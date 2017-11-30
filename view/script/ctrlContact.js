bc77774dd12e672432dcf4c0605da206ff1dacc2
SyracuseWiesler <gslzwiesler@gmail.com>
2017-11-30 13:52:23 -0800

    commit 7

--- view/script/ctrlContact.js
+++ view/script/ctrlContact.js
@@ -3,7 +3,9 @@ angular.module("myApp").controller("ctrlContact", ["$scope", "myFactory", "$root
     $rootScope.menubar = myFactory.menubar;
     $scope.flag = true;
     $scope.displayAlert = false;
+    $scope.clicked = false;
     $scope.submit = function(){
+        $scope.clicked = true;
         var email = {
             name: $scope.name,
             email: $scope.email,
@@ -14,18 +16,24 @@ angular.module("myApp").controller("ctrlContact", ["$scope", "myFactory", "$root
             // 显示您的信息已发送成功，我们会尽快与您联系
             console.log(res.data);
             $scope.flag = true;
+            $scope.clicked = false;
             $scope.displayAlert = true;
+            $scope.name = "";
+            $scope.email = "";
+            $scope.tel = "";
+            $scope.info = "";
             $timeout(function(){
                 $scope.displayAlert = false;
-            }, 2000);
+            }, 3000);
         }, function error(res){
             // 提示未发送成功，请稍后再试
             console.log(res.data);
             $scope.flag = false;
+            $scope.clicked = false;
             $scope.displayAlert = true;
             $timeout(function(){
                 $scope.displayAlert = false;
-            }, 2000);
+            }, 4000);
         });
     };
 }]);
